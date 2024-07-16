a6a10f2e777d22ca3f365902bfe279c2e0ed0f26
iamrushi30 <167416672+iamrushi30@users.noreply.github.com>
2024-07-16 10:30:34 +0000

    Conversion

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,31 +1,67 @@
-let score="33abc"
+// let score="33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber =Number(score)
-console.log( typeof valueInNumber);
-console.log( valueInNumber);
-
-
-//"33" => 33
-//"33abc"=>NaN not a number
-// true=> 1; false=>0 
-
-//let isLoggedIn="Rushi"
-
-//let booleanIsLoggedIn=Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// let valueInNumber =Number(score)
+// console.log( typeof valueInNumber);
+// console.log( valueInNumber);
 
 
-// 1=> true ; 0=>false
-// "" =>false
-// "Rushi"=>true
+// //"33" => 33
+// //"33abc"=>NaN not a number
+// // true=> 1; false=>0 
+
+// //let isLoggedIn="Rushi"
+
+// //let booleanIsLoggedIn=Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn);
+
+
+// // 1=> true ; 0=>false
+// // "" =>false
+// // "Rushi"=>true
 
 
 
-let someNumber=33
+// let someNumber=33
 
-let stringNumber=String(someNumber)
-console.log( stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber=String(someNumber)
+// console.log( stringNumber);
+// console.log(typeof stringNumber);
+
+
+//******************Opertaion************************ */
+
+let value =3
+let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" Rushi"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=3+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
