6d7544022975ac98330fa829cfd0ef260d79ffb0
iamrushi30 <167416672+iamrushi30@users.noreply.github.com>
2024-07-16 09:26:29 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // terat all JS code as newer version
+
+// alert(3+3) // we are using node js ,not browser
+
+console.log(3+3);
+
+// code readability should be high
+
+console.log("rushi");
+
+let name="Rushi"
+let age=18
+let isLoggedIn=false
+
+// number=> 2 to power
+// bigint
+// string=> ""
+//boolean=> true/false
+// null=> standalone value
+//undefined=>
+// symbol=> unique
+
+
+// object
+console.log(typeof undefined);//  undefined
+console.log(typeof null);// object
