f6add9287cd53efd43dd453b090191555914c0b7
iamrushi30 <167416672+iamrushi30@users.noreply.github.com>
2024-07-18 06:40:41 +0000

    summarry

---
+++ 01_Basics/04_DataTypeSummary.js
@@ -0,0 +1,35 @@
+/*  # primitive Datatype
+
+ 7 types : String , Number , Boolean ,Null , Undefined , Symbol , Bigint*/
+
+ const score=100
+ const scoreValue=100.3
+
+ const isLoggedIn=false
+ const outsideTemp=null;
+ let useremail;
+
+
+ const id=Symbol('123')
+ const anotherId=Symbol('123')
+
+ console.log(id==anotherId)
+
+ //const bigNumber=3232235113n
+
+
+ /* # Refrence (non primitive datatype )
+ Array , Objects , Functions
+ */  
+
+ const heros=["shaktiman","naagraj","doga"]
+ let myobj={
+    name:"rushi",
+    age:19,
+ }
+
+ const myFunction=function(){
+    console.log("hello world");
+ }
+
+ console.log(typeof bigNumber);
